6d96034ee13957af5ff35d0ab51910e34d0a2782
Obukhov Vsevolod <sevaobuxov61@gmail.com>
2023-11-13 17:07:24 +0300

    connect prisma and add auth module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private prisma: PrismaService){}
+
+    async register(dto: AuthDto){
+
+    }
+}
